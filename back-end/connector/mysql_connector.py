b97121565c59bb49836784d99c7dc2a0ac99a31b
ilham tenriajeng <ilhamtenriajeng03@gmail.com>
2024-07-31 20:23:32 +0800

    feat: add config and mysql connector

---
+++ back-end/connector/mysql_connector.py
@@ -0,0 +1,7 @@
+from sqlalchemy import create_engine 
+from config.config import Config
+ 
+print("Connecting to MySQL Database")
+engine = create_engine(Config.SQLALCHEMY_DATABASE_URI)
+connection = engine.connect()
+print("Success connecting to MySQL Database")
