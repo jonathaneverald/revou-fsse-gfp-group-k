f811d13363513cf930c5fa40d9b1e4e7ebd4b6af
ilham tenriajeng <ilhamtenriajeng03@gmail.com>
2024-08-01 10:23:40 +0800

    feat: add migration

--- back-end/app.py
+++ back-end/app.py
@@ -1,12 +1,31 @@
 from flask import Flask
-from dotenv import load_dotenv
-from connector.mysql_connector import connection
+from flask_migrate import Migrate  
 from config.config import Config
 from db import db
-
+from models.users import UserModel
+from sqlalchemy.orm import sessionmaker
+from flask_login import LoginManager 
+from connector.mysql_connector import connection 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
+    db.init_app(app)
+    login_manager = LoginManager()
+    Migrate(app, db)
+    
+    @login_manager.user_loader
+    def load_user(user_id):
+        Session = sessionmaker(connection)
+        s = Session()
+        return s.query(UserModel).get(int(user_id))
+    
+    with app.app_context():
+        db.create_all()
+    
     return app
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run(debug=True)
--- back-end/config/config.py
+++ back-end/config/config.py
@@ -3,7 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
 class Config:
     SQLALCHEMY_DATABASE_URI = f"mysql+mysqlconnector://{os.getenv('DB_USERNAME')}:{os.getenv('DB_PASSWORD')}@localhost/{os.getenv('DB_DATABASE')}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
