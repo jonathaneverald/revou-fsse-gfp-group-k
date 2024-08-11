572277ac50c307017ac2d2ff8c567291e7e4b3ce
ilham tenriajeng <ilhamtenriajeng03@gmail.com>
2024-08-11 15:07:58 +0800

    fix: fix bug redirect search form

--- front-end/src/components/form/SearchForm.tsx
+++ front-end/src/components/form/SearchForm.tsx
@@ -1,64 +1,65 @@
-import { LoaderCircle, Search } from "lucide-react";
-import React, { useState, useEffect } from "react";
+import { Loader, Search } from "lucide-react";
+import React, { useState, useCallback, useRef } from "react";
 import { Input } from "../ui/input";
 import { useRouter } from "next/router";
 
 const SearchForm: React.FC = () => {
 	const [searchTerm, setSearchTerm] = useState<string>("");
-	const [debouncedSearchTerm, setDebouncedSearchTerm] = useState<string>(searchTerm);
-	const [loading, setLoading] = useState<boolean>(false);
+	const [isLoading, setIsLoading] = useState<boolean>(false);
 	const router = useRouter();
+	const debounceTimeout = useRef<NodeJS.Timeout | null>(null);
 
 	const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
 		setSearchTerm(event.target.value);
 	};
 
-	const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-		event.preventDefault();
-		setLoading(true);
-		setDebouncedSearchTerm(searchTerm);
-	};
+	const handleSubmit = useCallback(
+		(event: React.FormEvent<HTMLFormElement>) => {
+			event.preventDefault();
+			setIsLoading(true);
 
-	useEffect(() => {
-		const handler = setTimeout(() => {
-			const isProductsPage = router.pathname === "/products";
-			const query = isProductsPage ? { ...router.query } : {};
-
-			if (debouncedSearchTerm) {
-				query.product_name = debouncedSearchTerm;
-			} else {
-				delete query.product_name;
+			if (debounceTimeout.current) {
+				clearTimeout(debounceTimeout.current);
 			}
 
-			router.push({
-				pathname: "/products",
-				query,
-			});
+			debounceTimeout.current = setTimeout(() => {
+				setIsLoading(true);
 
-			setLoading(false);
-		}, 1000);
+				const isProductsPage = router.pathname === "/products";
+				const query = isProductsPage ? { ...router.query } : {};
 
-		return () => {
-			clearTimeout(handler);
-		};
-	}, [debouncedSearchTerm, router.pathname]);
+				if (searchTerm) {
+					query.product_name = searchTerm;
+				} else {
+					delete query.product_name;
+				}
+
+				router
+					.push({
+						pathname: "/products",
+						query,
+					})
+					.finally(() => {
+						setIsLoading(false);
+					});
+			}, 1000);
+		},
+		[searchTerm, router]
+	);
 
 	return (
 		<form onSubmit={handleSubmit} className="ml-auto flex-1 sm:flex-initial">
 			<div className="relative">
-				{loading ? (
-					<LoaderCircle className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground animate-spin" />
-				) : (
-					<Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-				)}
+				<Search className="absolute left-2.5 top-3 h-4 w-4 text-muted-foreground" />
 				<Input
-					type="search"
+					type="text"
 					value={searchTerm}
 					onChange={handleSearchChange}
 					placeholder="Search products..."
-					disabled={loading}
 					className="pl-8 rounded-full focus:outline-none sm:w-[300px] md:w-[200px] lg:w-[300px]"
+					disabled={isLoading}
 				/>
+				{isLoading && <Loader className="animate-spin absolute right-2.5 top-3 h-4 w-4 " />}
 			</div>
 		</form>
 	);
