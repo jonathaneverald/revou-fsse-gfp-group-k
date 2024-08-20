131e2d638313f83e26b6d8726df12e254bc2e06b
ilham tenriajeng <ilhamtenriajeng03@gmail.com>
2024-08-20 15:50:34 +0800

    fix: update App component to conditionally render layout based on route

--- front-end/src/pages/_app.tsx
+++ front-end/src/pages/_app.tsx
@@ -1,18 +1,29 @@
-import "@/styles/globals.css";
-import "@/styles/embla.css";
-import type { AppProps } from "next/app";
-import Layout from "@/components/Layout";
-import { Toaster } from "@/components/ui/sonner";
-import { Provider } from "react-redux";
-import { store } from "@/store/store";
+import '@/styles/globals.css'
+import '@/styles/embla.css'
+import type { AppProps } from 'next/app'
+import Layout from '@/components/Layout'
+import { Toaster } from '@/components/ui/sonner'
+import { Provider } from 'react-redux'
+import { store } from '@/store/store'
+import { useRouter } from 'next/router'
 
 export default function App({ Component, pageProps }: AppProps) {
-	return (
-		<Provider store={store}>
-			<Layout>
-				<Component {...pageProps} />
-				<Toaster />
-			</Layout>
-		</Provider>
-	);
+    const router = useRouter()
+    const excludeLayout =
+        router.pathname === '/login' || router.pathname === '/register'
+    return (
+        <Provider store={store}>
+            {!excludeLayout ? (
+                <Layout>
+                    <Component {...pageProps} />
+                    <Toaster />
+                </Layout>
+            ) : (
+                <>
+                    <Component {...pageProps} />
+                    <Toaster />
+                </>
+            )}
+        </Provider>
+    )
 }
