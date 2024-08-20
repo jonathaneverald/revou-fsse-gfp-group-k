96b75b4a3258497bb501f4f4e60e31e09726a5e9
ilham tenriajeng <ilhamtenriajeng03@gmail.com>
2024-08-20 15:50:57 +0800

    feat: add registration form component with validation using react-hook-form and zod

---
+++ front-end/src/pages/register/index.tsx
@@ -0,0 +1,184 @@
+import React from 'react'
+import Link from 'next/link'
+import { Button } from '@/components/ui/button'
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import useRegister from '@/hooks/useRegister'
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from '@/components/ui/form'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
+
+const FormSchema = z.object({
+    name: z.string().min(2, {
+        message: 'Name must be at least 2 characters.',
+    }),
+    email: z.string().email('Invalid email address'),
+    password: z.string().min(6, 'Password must be at least 6 characters long'),
+    address: z.string().min(5, 'Address must be at least 5 characters long'),
+    phone_number: z
+        .string()
+        .min(10, 'Phone number must be at least 10 digits long'),
+})
+
+const Register: React.FC = () => {
+    const form = useForm<z.infer<typeof FormSchema>>({
+        resolver: zodResolver(FormSchema),
+        defaultValues: {
+            name: '',
+            email: '',
+            password: '',
+            address: '',
+            phone_number: '',
+        },
+    })
+
+    const { onSubmit, loading, error, success } = useRegister()
+
+    const handleFormSubmit = (data: z.infer<typeof FormSchema>) => {
+        onSubmit(data)
+    }
+
+    return (
+        <div className="flex min-h-screen items-center justify-center bg-gray-100 py-10">
+            <Card className="mx-auto max-w-sm md:w-1/3">
+                <CardHeader>
+                    <CardTitle className="text-2xl">Sign Up</CardTitle>
+                    <CardDescription>
+                        Enter your details below to create a new account
+                    </CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <Form {...form}>
+                        <form
+                            onSubmit={form.handleSubmit(handleFormSubmit)}
+                            className="grid gap-4"
+                        >
+                            <div className="grid gap-2">
+                                <FormField
+                                    control={form.control}
+                                    name="name"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormLabel>Name</FormLabel>
+                                            <FormControl>
+                                                <Input
+                                                    placeholder="Name"
+                                                    {...field}
+                                                />
+                                            </FormControl>
+                                            <FormMessage />
+                                        </FormItem>
+                                    )}
+                                />
+                            </div>
+                            <div className="grid gap-2">
+                                <FormField
+                                    control={form.control}
+                                    name="email"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormLabel>Email</FormLabel>
+                                            <FormControl>
+                                                <Input
+                                                    placeholder="Type your email here."
+                                                    {...field}
+                                                />
+                                            </FormControl>
+                                            <FormMessage />
+                                        </FormItem>
+                                    )}
+                                />
+                            </div>
+                            <div className="grid gap-2">
+                                <FormField
+                                    control={form.control}
+                                    name="password"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormLabel>Password</FormLabel>
+                                            <FormControl>
+                                                <Input
+                                                    type="password"
+                                                    placeholder="Type your password here."
+                                                    {...field}
+                                                />
+                                            </FormControl>
+                                            <FormMessage />
+                                        </FormItem>
+                                    )}
+                                />
+                            </div>
+                            <div className="grid gap-2">
+                                <FormField
+                                    control={form.control}
+                                    name="address"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormLabel>Address</FormLabel>
+                                            <FormControl>
+                                                <Input
+                                                    placeholder="Type your address here."
+                                                    {...field}
+                                                />
+                                            </FormControl>
+                                            <FormMessage />
+                                        </FormItem>
+                                    )}
+                                />
+                            </div>
+                            <div className="grid gap-2">
+                                <FormField
+                                    control={form.control}
+                                    name="phone_number"
+                                    render={({ field }) => (
+                                        <FormItem>
+                                            <FormLabel>Phone number</FormLabel>
+                                            <FormControl>
+                                                <Input
+                                                    placeholder="Type your phone number here."
+                                                    {...field}
+                                                />
+                                            </FormControl>
+                                            <FormMessage />
+                                        </FormItem>
+                                    )}
+                                />
+                            </div>
+
+                            <Button
+                                type="submit"
+                                className="w-full"
+                                disabled={loading}
+                            >
+                                {loading ? 'Registering...' : 'Sign Up'}
+                            </Button>
+                        </form>
+                    </Form>
+                    <div className="mt-4 text-center text-sm">
+                        Already have an account?{' '}
+                        <Link href="/login" className="underline">
+                            Login
+                        </Link>
+                    </div>
+                </CardContent>
+            </Card>
+        </div>
+    )
+}
+
+export default Register
