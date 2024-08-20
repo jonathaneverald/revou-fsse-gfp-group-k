06bc8b55fc12cf43df16c537ce32bb9ea2be4609
ilham tenriajeng <ilhamtenriajeng03@gmail.com>
2024-08-20 20:29:01 +0800

    fix: clean up and refactor ProductList component

--- front-end/src/components/products/ProductList.tsx
+++ front-end/src/components/products/ProductList.tsx
@@ -1,124 +1,140 @@
-import React, { useState, useEffect } from "react";
-import { useRouter } from "next/router";
-import ProductCard from "../card/ProductCard";
-import { Product } from "@/types/product";
-import useFetchProducts from "@/hooks/useFetchProducts";
-import ProductCartLoading from "../loading/ProductCartLoading";
+import React, { useState, useEffect } from 'react'
+import { useRouter } from 'next/router'
+import ProductCard from '../card/ProductCard'
+import { Product } from '@/types/product'
+import useFetchProducts from '@/hooks/useFetchProducts'
+import ProductCartLoading from '../loading/ProductCartLoading'
 import {
-  Pagination,
-  PaginationContent,
-  PaginationEllipsis,
-  PaginationItem,
-  PaginationLink,
-  PaginationNext,
-  PaginationPrevious,
-} from "@/components/ui/pagination";
+    Pagination,
+    PaginationContent,
+    PaginationEllipsis,
+    PaginationItem,
+    PaginationLink,
+    PaginationNext,
+    PaginationPrevious,
+} from '@/components/ui/pagination'
 
 const ProductList = () => {
-  const router = useRouter();
-  const {
-    category,
-    location,
-    product_name,
-    page = 1,
-    per_page = 12,
-  } = router.query;
+    const router = useRouter()
+    const {
+        category,
+        location,
+        product_name,
+        page = 1,
+        per_page = 12,
+    } = router.query
 
-  const [currentPage, setCurrentPage] = useState<number>(Number(page));
+    const [currentPage, setCurrentPage] = useState<number>(Number(page))
 
-  const { products, isLoading, isError } = useFetchProducts({
-    page: currentPage,
-    per_page: Number(per_page),
-    category: category as string,
-    location: location as string,
-    product_name: product_name as string,
-  });
+    const { products, isLoading, isError } = useFetchProducts({
+        page: currentPage,
+        per_page: Number(per_page),
+        category: category as string,
+        location: location as string,
+        product_name: product_name as string,
+    })
 
-  useEffect(() => {
-    if (Number(page) !== currentPage) {
-      setCurrentPage(Number(page));
+    useEffect(() => {
+        if (Number(page) !== currentPage) {
+            setCurrentPage(Number(page))
+        }
+    }, [page])
+
+    useEffect(() => {
+        if (Number(page) !== currentPage) {
+            router.push({
+                pathname: router.pathname,
+                query: { ...router.query, page: currentPage },
+            })
+        }
+    }, [currentPage])
+
+    const handlePageChange = (page: number) => {
+        setCurrentPage(page)
     }
-  }, [page]);
 
-  useEffect(() => {
-    if (Number(page) !== currentPage) {
-      router.push({
-        pathname: router.pathname,
-        query: { ...router.query, page: currentPage },
-      });
+    if (isLoading) {
+        return (
+            <div className="grid w-full grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-2 lg:grid-cols-4">
+                <ProductCartLoading />
+            </div>
+        )
     }
-  }, [currentPage]);
 
-  const handlePageChange = (page: number) => {
-    setCurrentPage(page);
-  };
+    if (isError) {
+        return <p>Failed to load products</p>
+    }
 
-  if (isLoading) {
+    const { total_pages, next_page, prev_page } = products.pagination
+
     return (
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-2 lg:grid-cols-4 gap-4 w-full">
-        <ProductCartLoading />
-      </div>
-    );
-  }
+        <div>
+            <div className="grid w-full grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-2 lg:grid-cols-4">
+                {products.data.map((product: Product) => (
+                    <ProductCard
+                        key={product.id}
+                        image={
+                            product.image_urls[0] ||
+                            'https://via.placeholder.com/300x200?text=No+Image'
+                        }
+                        name={product.name}
+                        product_slug={product.slug}
+                        description={product.description}
+                        originalPrice={product.price}
+                        type={product.type}
+                        seller_name={product.seller_name}
+                        seller_slug={product.seller_slug}
+                    />
+                ))}
+            </div>
+            <div className="mt-5">
+                <Pagination>
+                    <PaginationContent>
+                        <PaginationItem>
+                            <PaginationPrevious
+                                href="#"
+                                onClick={() => handlePageChange(prev_page || 1)}
+                            />
+                        </PaginationItem>
+                        {[...Array(total_pages)].map((_, index) => {
+                            const page = index + 1
+                            const startPage = Math.max(1, currentPage - 2)
+                            const endPage = Math.min(
+                                total_pages,
+                                currentPage + 2
+                            )
 
-  if (isError) {
-    return <p>Failed to load products</p>;
-  }
+                            if (page >= startPage && page <= endPage) {
+                                return (
+                                    <PaginationItem key={index}>
+                                        <PaginationLink
+                                            href="#"
+                                            isActive={page === currentPage}
+                                            onClick={() =>
+                                                handlePageChange(page)
+                                            }
+                                        >
+                                            {page}
+                                        </PaginationLink>
+                                    </PaginationItem>
+                                )
+                            }
+                            return null
+                        })}
 
-  const { total_pages, next_page, prev_page } = products.pagination;
+                        <PaginationItem>
+                            <PaginationNext
+                                href="#"
+                                onClick={() =>
+                                    handlePageChange(next_page || total_pages)
+                                }
+                            />
+                        </PaginationItem>
+                    </PaginationContent>
+                </Pagination>
+            </div>
+        </div>
+    )
+}
 
-  return (
-    <div>
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-2 lg:grid-cols-4 gap-4 w-full">
-        {products.data.map((product: Product) => (
-          <ProductCard
-            key={product.id}
-            image={
-              product.image_urls[0] ||
-              "https://via.placeholder.com/300x200?text=No+Image"
-            }
-            name={product.name}
-            product_slug={product.slug}
-            description={product.description}
-            originalPrice={product.price}
-            type={product.type}
-            seller_name={product.seller_name}
-            seller_slug={product.seller_slug}
-          />
-        ))}
-      </div>
-      <div className="mt-5">
-        <Pagination>
-          <PaginationContent>
-            <PaginationItem>
-              <PaginationPrevious
-                href="#"
-                onClick={() => handlePageChange(prev_page || 1)}
-              />
-            </PaginationItem>
-            {[...Array(total_pages)].map((_, index) => (
-              <PaginationItem key={index}>
-                <PaginationLink
-                  href="#"
-                  isActive={index + 1 === currentPage}
-                  onClick={() => handlePageChange(index + 1)}
-                >
-                  {index + 1}
-                </PaginationLink>
-              </PaginationItem>
-            ))}
-            {total_pages > 3 && <PaginationEllipsis />}
-            <PaginationItem>
-              <PaginationNext
-                href="#"
-                onClick={() => handlePageChange(next_page || total_pages)}
-              />
-            </PaginationItem>
-          </PaginationContent>
-        </Pagination>
-      </div>
-    </div>
-  );
-};
-
-export default ProductList;
+export default ProductList
--- front-end/src/hooks/useFetchProducts.tsx
+++ front-end/src/hooks/useFetchProducts.tsx
@@ -1,45 +1,56 @@
-import { getToken } from "@/utils/tokenUtils";
-import useSWR from "swr";
+import { getToken } from '@/utils/tokenUtils'
+import useSWR from 'swr'
+import useDebounce from './useDebounce'
+
+type FetchProductsParams = {
+    page: number
+    per_page: number
+    category?: string
+    location?: string
+    product_name?: string
+}
 
 const fetcher = async (url: string) => {
-	const token = getToken();
-	return await fetch(url, {
-		headers: {
-			Authorization: `Bearer ${token}`,
-		},
-	}).then((res) => res.json());
-};
+    const token = getToken()
+    const response = await fetch(url, {
+        headers: {
+            Authorization: `Bearer ${token}`,
+        },
+    })
+    return response.json()
+}
 
-const useFetchProducts = ({
-	page,
-	per_page,
-	category = "",
-	location = "",
-	product_name = "",
-}: {
-	page: number;
-	per_page: number;
-	category?: string;
-	location?: string;
-	product_name?: string;
-}) => {
-	const params = new URLSearchParams({
-		page: page.toString(),
-		per_page: per_page.toString(),
-		category,
-		location,
-		product_name,
-	}).toString();
+const buildUrl = (params: FetchProductsParams): string => {
+    const {
+        page,
+        per_page,
+        category = '',
+        location = '',
+        product_name = '',
+    } = params
 
-	const url = `http://127.0.0.1:5000/product?${params}`;
+    const queryParams = new URLSearchParams({
+        page: page.toString(),
+        per_page: per_page.toString(),
+        category,
+        location,
+        product_name,
+    })
 
-	const { data, error } = useSWR(url, fetcher);
+    return `http://127.0.0.1:5000/product?${queryParams.toString()}`
+}
 
-	return {
-		products: data?.data,
-		isLoading: !error && !data,
-		isError: error,
-	};
-};
+const useFetchProducts = (params: FetchProductsParams) => {
+    const debouncedParams = useDebounce(params, 300)
 
-export default useFetchProducts;
+    const url = buildUrl(debouncedParams)
+    const { data, isLoading, error } = useSWR(url, fetcher)
+
+    return {
+        products: data?.data,
+        isLoading: isLoading,
+        isError: error,
+    }
+}
+
+export default useFetchProducts
