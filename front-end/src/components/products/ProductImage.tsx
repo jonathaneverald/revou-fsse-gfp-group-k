ad8238c3b2d496f72954c96164ee917f4a46ac5f
ilham tenriajeng <ilhamtenriajeng03@gmail.com>
2024-08-22 13:27:20 +0800

    style: update image product detail UI

--- front-end/src/components/products/ProductImage.tsx
+++ front-end/src/components/products/ProductImage.tsx
@@ -1,41 +1,44 @@
-import Image from "next/image";
+import Image from 'next/image'
 import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "../ui/carousel";
-import { ProductImagesProps } from "@/types/product";
+    Carousel,
+    CarouselContent,
+    CarouselItem,
+    CarouselNext,
+    CarouselPrevious,
+} from '../ui/carousel'
+import { ProductImagesProps } from '@/types/product'
 
 const ProductImage: React.FC<ProductImagesProps> = ({ images }) => {
-  // Provide a default placeholder image if no images are available
-  const defaultImage = "https://via.placeholder.com/400x400?text=No+Image";
-  const displayImages = images.length > 0 ? images : [defaultImage];
+    // Provide a default placeholder image if no images are available
+    const defaultImage = 'https://via.placeholder.com/400x400?text=No+Image'
+    const displayImages = images.length > 0 ? images : [defaultImage]
 
-  return (
-    <div className="flex justify-center">
-      <Carousel opts={{ align: "start" }} className="h-full max-w-sm">
-        <CarouselContent>
-          {displayImages.map((image, index) => (
-            <CarouselItem key={index} className="w-full ">
-              <div className="p-1">
-                <Image
-                  className="aspect-square w-full object-cover rounded-md overflow-hidden"
-                  src={image}
-                  width={400}
-                  height={400}
-                  alt={`product image ${index + 1}`}
-                />
-              </div>
-            </CarouselItem>
-          ))}
-        </CarouselContent>
-        <CarouselPrevious className="left-3" />
-        <CarouselNext className="right-3" />
-      </Carousel>
-    </div>
-  );
-};
+    return (
+        <div className="flex justify-center">
+            <Carousel
+                opts={{ align: 'start' }}
+                className="h-full w-full max-w-sm"
+            >
+                <CarouselContent>
+                    {displayImages.map((image, index) => (
+                        <CarouselItem key={index} className="w-full">
+                            <div className="p-1">
+                                <Image
+                                    className="aspect-square w-full overflow-hidden rounded-md object-contain"
+                                    src={image}
+                                    width={400}
+                                    height={400}
+                                    alt={`product image ${index + 1}`}
+                                />
+                            </div>
+                        </CarouselItem>
+                    ))}
+                </CarouselContent>
+                <CarouselPrevious className="left-3" />
+                <CarouselNext className="right-3" />
+            </Carousel>
+        </div>
+    )
+}
 
-export default ProductImage;
+export default ProductImage
--- front-end/src/pages/products/[slug]/index.tsx
+++ front-end/src/pages/products/[slug]/index.tsx
@@ -1,78 +1,80 @@
-import React from "react";
-import dynamic from "next/dynamic";
-import { GetServerSideProps } from "next";
-import ProductImage from "@/components/products/ProductImage";
-import ProductInfo from "@/components/products/ProductInfo";
-import useFetchProductDetail from "@/hooks/useFetchProductDetail";
-import { ProductDetailProps } from "@/types/product";
+import React from 'react'
+import dynamic from 'next/dynamic'
+import { GetServerSideProps } from 'next'
+import ProductImage from '@/components/products/ProductImage'
+import ProductInfo from '@/components/products/ProductInfo'
+import useFetchProductDetail from '@/hooks/useFetchProductDetail'
+import { ProductDetailProps } from '@/types/product'
 
 const DynamicBreadcrumb = dynamic(
-  () =>
-    import("@/components/menu/DynamicBreadcrumb").then(
-      (mod) => mod.DynamicBreadcrumb
-    ),
-  { ssr: false }
-);
+    () =>
+        import('@/components/menu/DynamicBreadcrumb').then(
+            (mod) => mod.DynamicBreadcrumb
+        ),
+    { ssr: false }
+)
 
 export const getServerSideProps: GetServerSideProps<
-  ProductDetailProps
+    ProductDetailProps
 > = async (context) => {
-  const slug = context.query.slug as string;
+    const slug = context.query.slug as string
 
-  const authToken = context.req.cookies.jwtToken;
+    const authToken = context.req.cookies.jwtToken
 
-  try {
-    const product = await useFetchProductDetail(slug, authToken as string);
+    try {
+        const product = await useFetchProductDetail(slug, authToken as string)
 
-    return { props: { product: product.data } };
-  } catch (error) {
-    return { props: { error: (error as Error).message } };
-  }
-};
+        return { props: { product: product.data } }
+    } catch (error) {
+        return { props: { error: (error as Error).message } }
+    }
+}
 
 const ProductDetail: React.FC<ProductDetailProps> = ({ product, error }) => {
-  if (error) {
-    return <p className="text-red-500">Failed to load product</p>;
-  }
+    if (error) {
+        return <p className="text-red-500">Failed to load product</p>
+    }
 
-  if (!product) {
-    return <p>Product not found</p>;
-  }
+    if (!product) {
+        return <p>Product not found</p>
+    }
 
-  const {
-    slug,
-    category_name,
-    description,
-    name,
-    price,
-    seller_name,
-    type,
-    location_city,
-    image_urls,
-  } = product;
+    const {
+        slug,
+        category_name,
+        description,
+        name,
+        price,
+        seller_name,
+        type,
+        location_city,
+        image_urls,
+    } = product
 
-  return (
-    <div className="mx-4 md:container">
-      <DynamicBreadcrumb />
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-10 bg-white rounded-md p-10">
-        <ProductImage
-          images={Array.isArray(image_urls) ? image_urls : [image_urls]}
-        />
-        <ProductInfo
-          name={name}
-          price={price}
-          category={category_name}
-          categorySlug={category_name}
-          seller={seller_name}
-          sellerSlug={seller_name}
-          description={description}
-          type={type}
-          productSlug={slug}
-          locationCity={location_city}
-        />
-      </div>
-    </div>
-  );
-};
+    return (
+        <div className="mx-4 md:container">
+            <DynamicBreadcrumb />
+            <div className="mb-10 grid grid-cols-1 gap-4 rounded-md bg-white p-10 md:grid-cols-2">
+                <ProductImage
+                    images={
+                        Array.isArray(image_urls) ? image_urls : [image_urls]
+                    }
+                />
+                <ProductInfo
+                    name={name}
+                    price={price}
+                    category={category_name}
+                    categorySlug={category_name}
+                    seller={seller_name}
+                    sellerSlug={seller_name}
+                    description={description}
+                    type={type}
+                    productSlug={slug}
+                    locationCity={location_city}
+                />
+            </div>
+        </div>
+    )
+}
 
-export default ProductDetail;
+export default ProductDetail
